9b31a47ab85753756d7fb7356e18d8977c05f58f
Slava Kim <slava@meteor.com>
2014-08-31 18:28:52 -0700

    Save last entered bill for 5m

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+
+        // Set the tip from defaults
         var defaults = NSUserDefaults.standardUserDefaults()
         var tipOpt = defaults.integerForKey("defaultTipOption")
 
@@ -30,6 +32,15 @@ class ViewController: UIViewController {
         }
 
         tipControl.selectedSegmentIndex = tipOpt
+
+        // Set the bill from last session, if it was entered less than 5m ago
+        var lastEnteredDate = defaults.objectForKey("lastEnteredDate") as NSDate?
+
+        if lastEnteredDate !== nil && lastEnteredDate?.timeIntervalSinceNow > -60 * 5 {
+            billField.text = defaults.stringForKey("lastEnteredAmount")
+        }
+
+        onEditingChanged(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,6 +58,14 @@ class ViewController: UIViewController {
 
         tipLabel.text = String(format: "$%.2f", tipAmount)
         totalLabel.text = String(format: "$%.2f", totalAmount)
+
+        // Save the last entered amount
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(billField.text, forKey: "lastEnteredAmount")
+        defaults.setObject(NSDate(), forKey: "lastEnteredDate")
+        // Don't flush the defaults here, it sounds like a bad idea
+        // to force it on every key stroke, let's leave it to the
+        // Framework to decide when is a good time
     }
 
     @IBAction func onTap(sender: AnyObject) {
